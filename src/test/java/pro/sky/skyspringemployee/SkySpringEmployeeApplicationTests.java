66dbbe0a13d20a6bc82b7f0ce7923e8c9fdb504a
shaft163 <shaft163rus@gmail.com>
2023-07-16 20:40:08 +0400

    Initial commit

---
+++ src/test/java/pro/sky/skyspringemployee/SkySpringEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyspringemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkySpringEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
